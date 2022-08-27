7dbe11d1280166c704ba0c69c0f5730bbb507ccc
mananuf <bmananaf@gmail.com>
2022-08-27 20:57:27 +0100

    added click event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { artistes } from './artiste/artiste';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-components';
+  title = 'Artistry';
+  query: string;
+  artistes = artistes;
+
+  showArtiste(artiste:any){
+    this.query = artiste.name
+  }
+  constructor(){
+    this.query = ''
+  }
 }
