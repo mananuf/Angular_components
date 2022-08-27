7049858aa86b0bfa84350d7930739776ce2faee0
mananuf <bmananaf@gmail.com>
2022-08-27 23:47:17 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { artistes } from './artiste/artiste';
+import { ArtisteInterface } from './artiste/artisteInterface';
+import { ArtisteService } from './services/artiste.service';
+// import { artistes } from './artiste/artiste.ts';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Artistry';
   query: string;
-  artistes = artistes;
+  artistes:ArtisteInterface[] = [];
 
   showArtiste(artiste:any){
     this.query = artiste.name
+    artiste.highlight = !artiste.highlight
   }
-  constructor(){
+  constructor(private artisteService: ArtisteService){
     this.query = ''
+
+  }
+
+  ngOnInit(): void {
+    // get data from server and pass the data into artistes
+    this.artisteService.getArtiste().subscribe((data) => (this.artistes = data))
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
+// modules
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
+
+// components
 import { AppComponent } from './app.component';
+// services
+
+import { ArtisteService } from './services/artiste.service';
+import { ArtisteItemComponent } from './components/artiste-item/artiste-item.component';
+import { SearchArtistePipe } from './pipes/search-artiste.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtisteItemComponent,
+    SearchArtistePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ArtisteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
